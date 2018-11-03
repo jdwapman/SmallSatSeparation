82ecef219a4d1427c219ef4ff23fb5b3d77b99a3
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-03 10:46:35 -0700

    Added placeholder constants

---
+++ SmallSatOptimization.m
@@ -0,0 +1,12 @@
+%% Set Up Constants
+
+% Earth's gravitational constant
+ue = 3.986004418e14;  % m^3s^-2
+GM = ue;  % Alternative representation
+
+% Atmospheric drag constants
+Cd   =  0;  % Satellite drag coefficient
+A    =  0;  % Surface area exposed to the incident stream
+m    =  0;  % Satellite mass
+rho  =  0;  % Atmospheric density at the satellite position
+vrel =  0;  % Velocity of satellite relative to the atmosphere
