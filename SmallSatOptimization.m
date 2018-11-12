3c6d8e2baa9dd751742c7352139fcbf873ae394f
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-11 22:04:25 -0800

    Moved constants back to main file as global variables

--- SmallSatOptimization.m
+++ SmallSatOptimization.m
@@ -16,9 +16,74 @@ clear
 
 %% Set Up Constants
 
-filename = 'Constants.mat'
-WriteSimulationConstants(filename) 
+% Note, must use global variables so they are available to the functions in
+% the nonlinear constraints. Keeping these variables in memory is faster
+% than saving to a file
+
+% Earth's gravitational constant. (Wikipedia)
+global ue;
+ue = 3.986004418e14;  % m^3s^-2
+
+% Satellite drag coefficient. (Li & Mason)
+global Cd;
+Cd = 2.2;  
+
+% Surface area exposed to the incident stream. (Li & Mason)
+% Amin < A < Amax
+global Amin Amax
+Amin = 0.0371; % (m^2)
+Amax = 0.225;  % (m^2)
+
+% Satellite mass. (Li & Mason)
+global m;
+m = 4.9;  % (kg)
+
+% Velocity of satellite relative to the atmosphere
+global vrel;
+vrel = 0;  % (m/sec)
+
+% Atmospheric density at the satellite position 
+global rho;
+rho = 2.5e-12;  % (kg/m^3)
 
 %% Simulation Parameters
 
- 
+% Number of satellites in the simulation
+global N;
+N = 105;
+
+% Number of time steps/input commands
+global T;
+T = 71;  % (Days)
+
+% Initial position
+global theta0;
+theta0 = zeros(N,1);  % Preallocate for speed
+theta0(:) = 0;      % Set to initial conditions
+
+% Initial altitude
+global r0;
+r0 = zeros(N,1);  % Preallocate for speed
+r0(:) = 475000;   % (m)
+
+% Initial velocity
+global w0;
+w0 = zeros(N,1);
+w0(:) = sqrt(ue./r0.^3);
+
+% Update time
+global dt;
+dtDays = 1;
+dt = dtDays * 24 * 60 * 60;
+
+% Position tolerance
+global epsTheta;
+epsTheta = 0.1;  % (rad)
+
+% Velocity tolerance
+global epsOmega;
+epsOmega = 1e-18;  % (rad/sec)
+
+%% Recreate using linear programming
+
+%% Recreate using nonlinear optimization
