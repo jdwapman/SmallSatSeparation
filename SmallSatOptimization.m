35d73e8765a3d82ecff719330a1cf0164bf68625
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-11 21:38:12 -0800

    Writes simulation constants to a .mat file so they can be used globally

--- SmallSatOptimization.m
+++ SmallSatOptimization.m
@@ -1,5 +1,14 @@
 %% Small Satellite Optimization
-% DESCRIPTIVE TEXT
+%
+% This project 1) replicates and 2) extends the work performed in Sin's
+% paper with the goal of determining whether nonlinear programming delivers
+% significant performance improvements compared to the linear programming
+% performed in the reference work. 
+%
+% E. Sin, M. Arcak, and A. Packard, “Small Satellite Constellation
+% Separation using Linear Programming based Differential Drag Commands,”
+% arXiv:1710.00104 [cs], Sep. 2017.
+
 
 %% Initialization Steps
 clc
@@ -7,46 +16,9 @@ clear
 
 %% Set Up Constants
 
-% Earth's gravitational constant. (Wikipedia)
-ue = 3.986004418e14;  % m^3s^-2
-GM = ue;  % Alternative representation
-
-% Satellite drag coefficient. (Li & Mason)
-Cd = 2.2;  
-
-% Surface area exposed to the incident stream. (Li & Mason)
-% Amin < A < Amax
-Amax = 0.225;  % (m^2)
-Amin = 0.0371; % (m^2)
-
-% Satellite mass. (Li & Mason)
-m = 4.9;  % (kg)
-
-% Velocity of satellite relative to the atmosphere
-vrel = 0;  % (m/sec)
-
-% Atmospheric density at the satellite position (kg/m^3)
-rho = 2.5e-12;  
+filename = 'Constants.mat'
+WriteSimulationConstants(filename) 
 
 %% Simulation Parameters
-
-% Number of satellites
-N = 105; 
-
-% Initial theta
-theta_0 = zeros(N,1);
-
-% r_0
-% TODO
-
-% omega_0
-% TODO
-
-% Circular orbit altitude
-h = 475 * 10^3;  % (m)
-
-% Theta tolerance
-eps_theta = 0.1;  % Degrees
-eps_omega = 1e-18;  % Rad/sec
 
  
