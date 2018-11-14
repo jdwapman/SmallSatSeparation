18a139a6d8fc58eea0103b95afb411fa21aa807f
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-14 11:53:54 -0800

    Added placeholder function for atmospheric density rho

---
+++ rho.m
@@ -0,0 +1,20 @@
+function [density] = rho(r)
+%rho Returns the atmospheric density at the altitude r
+%   Uses a simplified version of the Harris-Priester model, as described by
+%   Montenbruck, that does not include diurnal effects.
+%
+%   O. Montenbruck and E. Gill, Satellite Orbits. New York: Springer,
+%   2005, ch. 3.
+%
+%   Currently, we do not have access to this book so this function is
+%   a placeholder.
+%
+%   Inputs:
+%       r: Satellite altitude [N x 1] (m)
+%   Outputs:
+%       density: atmospheric density at the given height [N x 1] (unknown)
+
+density = ones(size(r));
+
+end
+
