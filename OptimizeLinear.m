d45421871572d0b46d55f80f81a5aac34f6421be
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-19 12:11:12 -0800

    Added standalone function for linear optimization

---
+++ OptimizeLinear.m
@@ -0,0 +1,7 @@
+function [] = OptimizeLinear()
+%OptimizeLinear Performs linear optimization of the constellation
+%separation problem
+%   Performs linear optimization of the constellation separation problem
+
+end
+
