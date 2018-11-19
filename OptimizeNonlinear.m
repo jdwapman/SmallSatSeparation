f29a1720818c44925c251d952b0602208e6c5160
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-19 12:12:00 -0800

    Added standalone function for nonlinear optimization

---
+++ OptimizeNonlinear.m
@@ -0,0 +1,7 @@
+function [] = OptimizeNonlinear()
+%OptimizeLinear Performs nonlinear optimization of the constellation
+%separation problem
+%   Performs nonlinear optimization of the constellation separation problem
+
+end
+
