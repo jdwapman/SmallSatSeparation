360a390807b7d8e3eff779bc085e8ebd339a6e59
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-19 12:07:43 -0800

    Added description

--- SmallSatOptimization.m
+++ SmallSatOptimization.m
@@ -1,5 +1,12 @@
 %% Small Satellite Optimization
-% Project Description
+% This project 1) Replicates the linear-programming-based optimization done
+% by Sin et al. and 2) modifies the optimization problem to include nonlinear
+% constraints with the goal of evaluating the performance and computational
+% complexity of the two optimization methods.
+% 
+% E. Sin, M. Arcak, and A. Packard, “Small Satellite Constellation Separation
+% using Linear Programming based Differential Drag Commands,”
+% arXiv:1710.00104 [cs], Sep. 2017.
 
 %% Initialization Steps
 clc
