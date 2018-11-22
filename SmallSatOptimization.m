ace910672d09ea45a6b9e4650513975b70024049
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-21 23:00:43 -0800

    Added some optimization methods

--- SmallSatOptimization.m
+++ SmallSatOptimization.m
@@ -50,11 +50,11 @@ phi = deg2rad(90);  % 90 degrees for near-polar orbit
 
 % Number of satellites in the simulation
 global N;
-N = 1 % 105;
+N = 105; % 105;
 
 % Number of time steps/input commands
 global T;
-T = 100;  % (Days)
+T = 71;  % (Days)
 
 % Initial position
 global theta0;
@@ -105,20 +105,62 @@ delta_des(end) = -2*pi/N*(N-1);  % Replace last value
 u = repmat(Amin, N, T);
 [rRef, wRef, thetaRef] = trajectory(u);
 
+% 2) Precompute Sr and Somega matrices
+Sr_ref = [];
+Somega_ref = [];
+Salpha_ref = [];
+alpha = ((T-1/2)*ones(1,T) - (0:1:T-1));
+for n = 1:1:N
+    Sr_i = Sr(rRef(n,:), wRef(n,:))';  % Must convert to row vector
+    Sr_ref = blkdiag(Sr_ref, Sr_i);
+    
+    Somega_i = Somega(rRef(n,:), wRef(n,:))';  % Must convert to row vector
+    Somega_ref = blkdiag(Somega_ref, Somega_i);
+    
+    Salpha_i = alpha .* Somega_i;
+    Salpha_ref = blkdiag(Salpha_ref, Salpha_i);
+end
+
+
+% 4) Create A matrix for linear programming
+A = [-dt*Sr_ref, -ones(N,1); ...
+    dt^2*D*Salpha_ref, zeros(N,1); ...
+    -dt^2*D*Salpha_ref, zeros(N,1); ...
+    dt*D*Somega_ref, zeros(N,1); ...
+    -dt*D*Somega_ref, zeros(N,1); ...
+    eye(N*T, N*T), zeros(N*T, 1); ...
+    -eye(N*T, N*T), zeros(N*T, 1)];
+
+b = [r0; ...
+    epsTheta * ones(N,1) - D*(theta0 + dt*T*w0)+delta_des; ...
+    epsTheta * ones(N,1) + D*(theta0 + dt*T*w0)-delta_des; ...  
+    epsOmega * ones(N,1) - D*w0; ...
+    epsOmega * ones(N,1) + D*w0; ...
+    Amax * ones(N*T, 1); ...
+    -Amin * ones(N*T, 1)];
+
+% Create cost function
+f = [zeros(1,N*T), 1];
+x0 = [repmat(Amin, N*T, 1); 100e3];
+
+costfun = @(x)f*x;
+
+% Optimize
+result = fmincon(costfun, x0, A, b);
 
 % Plot reference trajectories
-t = 1:1:T;
-figure
-plot(t,rRef/1000)
-xlabel("Time (Days)")
-ylabel("Altitude (km)")
-title("Altitude vs Time")
-
-figure
-plot(t, rad2deg(wRef))
-xlabel("Time (Days)")
-ylabel("Angular Velocity (deg)")
-title(" Angular Velocity vs Time")
+% t = 1:1:T;
+% figure
+% plot(t,rRef/1000)
+% xlabel("Time (Days)")
+% ylabel("Altitude (km)")
+% title("Altitude vs Time")
+% 
+% figure
+% plot(t, rad2deg(wRef))
+% xlabel("Time (Days)")
+% ylabel("Angular Velocity (deg)")
+% title(" Angular Velocity vs Time")
 
 
 %% Recreate using nonlinear optimization
