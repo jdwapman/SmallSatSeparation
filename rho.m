d231526228a0bbfe681e2f9f71549d7e998f160c
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-11-27 22:29:17 -0800

    Comment tweaks

--- rho.m
+++ rho.m
@@ -6,9 +6,6 @@ function [density] = rho(r)
 %   O. Montenbruck and E. Gill, Satellite Orbits. New York: Springer,
 %   2005, ch. 3.
 %
-%   Currently, we do not have access to this book so this function is
-%   a placeholder.
-%
 %   Inputs:
 %       r: Satellite altitude [N x 1] (m)
 %   Outputs:
